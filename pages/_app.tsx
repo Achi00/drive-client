73934423c5b98fda1b0621d9957392cf1c9cb92c
Achi00 <toolvlg@gmail.com>
2024-06-01 02:16:05 +0400

    hide sidebar if no user

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import { userTypes } from "@/types"; // Adjust the import path accordingly
 import ProtectedRoute from "@/utils/ProtectedRoute";
 
 interface MyAppProps extends AppProps {
-  user: userTypes;
+  user: userTypes | null; // Allow user to be null when not authenticated
 }
 
 function MyApp({ Component, pageProps }: MyAppProps) {
@@ -16,10 +16,14 @@ function MyApp({ Component, pageProps }: MyAppProps) {
   const unprotectedRoutes = ["/file/[id]"];
 
   const isProtectedRoute = !unprotectedRoutes.includes(router.pathname);
+  const user = pageProps.user; // Destructure user from pageProps
+
+  console.log("User:", user);
 
   return (
     <div className="flex">
-      <Sidebar user={pageProps.user} />
+      {!user?.error && <Sidebar user={user} />}
+      {/* Render Sidebar only if user exists */}
       <main className="flex-grow">
         {isProtectedRoute ? (
           <ProtectedRoute>
