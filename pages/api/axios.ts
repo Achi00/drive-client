2ef1634a0472d08af197e0f937d412fa171b3cdb
Achi00 <toolvlg@gmail.com>
2024-11-02 00:27:20 +0400

    modify axios.ts

--- pages/api/axios.ts
+++ pages/api/axios.ts
@@ -4,6 +4,15 @@ const api = axios.create({
   baseURL: "https://driveapi.wordcrafter.io",
   // baseURL: "http://localhost:8080",
   withCredentials: true, // important for sending cookies with requests
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  config.withCredentials = true;
+  return config;
 });
 
 export default api;
